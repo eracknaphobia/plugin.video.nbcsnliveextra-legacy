777aa3d6ddd13ecf1dbc4e2a08e2c2f4d3b09f13
eracknaphobia <eracknaphobia@hotmail.com>
2015-09-04 11:21:46 -0400

    Update cox.py

--- resources/providers/cox.py
+++ resources/providers/cox.py
@@ -42,19 +42,7 @@ class COX():
         #cj.save(ignore_discard=True);                
         SAVE_COOKIE(cj)        
 
-        #First Redirect url
-        '''
-        https://idm.east.cox.net/affwebservices/public/saml2sso?SAMLRequest=lVNNj5swEL33VyDfDYGQlliBVbrpqpG2LU3YHnqpDAyNJbCpx2STf18DSTfd1aaq5NNo3sfMGy9uDk3t7EGjUDImvjshDshClUL%2BjMlDdkcjcpO8WSBv6qBly87s5AZ%2BdYDGWSKCNhZ3qyR2Degt6L0o4GFzH5OdMS0yz8PW5Rbk8lLl4BaqsRWvZ%2FO2y0%2F3LyiIs7LUQnIz%2BDmziLJxgaOxBAdXgmFhOPV4VT1CjqMoem2X16IYuANERZw7pQsYHMek4jUCcdarmPwoSj%2F05%2Fwdzblf0TCMfDqHgNNplJezyJ%2B%2FnUWVbcWUI4o9PIERO1hLNFyamAQTf0YnEZ36mR%2BxcGKfO4%2Bm34mTamVUoer3Qo5r7LRkiqNAJnkDyEzB%2BuFZ4E5YPjYh%2B5hlKU2%2FbDPifDvHEfRx2IAksjGA61ztSZgkY15scKwvGa4T8HMcJPmTn0W5L0NUkuaw43VFVeXJvMBWaYML71I3OV%2FNh4MB2U%2BEycLsdGuObAMWIctMncyN5dfdnUdjcDC2WeiStlyb47%2BMLrzngsnJ5N%2B2TqXPVnS9SpU9pKOzrGv1eKuBG3sDRncwnFTDzfUl9hVR0mpoZUZzqwHSEGeb9vRfO16LSoC%2B%2BCP%2Fs2PiPU1w%2BR2T3w%3D%3D&RelayState=_1de361bf-83fb-4f72-a0ad-3491cb2f12ce&SigAlg=http%3A%2F%2Fwww.w3.org%2F2000%2F09%2Fxmldsig%23rsa-sha1&Signature=F6Rh1sOZ2%2FknoXQYJ6JMLQwZRO5GDSAcXF5wD28uCnaLf4sEo0vflx6bqyux3wPNzzXGBVOIyh6bBEoCJATsl6COZrB4SB9jLkLGosIWYBctg6luq2O%2FQwqQbcv5UfzNdUoqh1KyrEtoamM8cav6pMqiY9GKrUbNE1EDNzrIFLC7XwcTq0pMcpiRoMt8TWLnP9dNtiLspv2CJW5c2GmzVAIrOUyURIBPsv5y5nYIILfgGRmJ9YoAY57zDxAw0cWqY401UQcQQ6S3GUnN3gdDcbO0eJPl%2BVFA6d6eUuMLNkNz6UGCzgZxgg%2FFUKYnB43uhUnnz%2BrIJmrIScrOD9NgKw%3D%3D HTTP/1.1
-        Host: idm.east.cox.net
-        Connection: keep-alive
-        Proxy-Connection: keep-alive
-        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-        User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 8_4 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12H143
-        Accept-Language: en-us
-        Referer: https://sp.auth.adobe.com//adobe-services/1.0/authenticate/saml?domain_name=adobe.com&noflash=true&mso_id=Cox&requestor_id=nbcsports&no_iframe=true&client_type=iOS&client_version=1.9&redirect_url=http://adobepass.ios.app/
-        Accept-Encoding: gzip, deflate
-        '''
-        
+      
         url = FIND(idp_source,'"0;url=','"')
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj)) 
         opener.addheaders = [ ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"),
@@ -111,7 +99,6 @@ class COX():
                             ("Content-Type", "application/x-www-form-urlencoded"),
                             ("Proxy-Connection", "keep-alive"),
                             ("Connection", "keep-alive"),
-                            #("Origin", "https://ids.rr.com"),
                             ("Referer", self.REFERER_URL),
                             ("User-Agent", UA_IPHONE)]
 
@@ -145,9 +132,7 @@ class COX():
             saml_response = HTMLParser.HTMLParser().unescape(saml_response)                                                        
             relay_state = FIND(idp_source,'<input type="hidden" name="RelayState" value="','"')
 
-            #Set Global header fields         
-            #ORIGIN = 'https://idm.east.cox.net'
-            #REFERER = 'https://idm.east.cox.net/affwebservices/public/saml2sso?SMASSERTIONREF=QUERY&RelayState=_1de361bf-83fb-4f72-a0ad-3491cb2f12ce&SAMLRequest=lVNNj5swEL33VyDfDYGQlliBVbrpqpG2LU3YHnqpDAyNJbCpx2STf18DSTfd1aaq5NNo3sfMGy9uDk3t7EGjUDImvjshDshClUL%2BjMlDdkcjcpO8WSBv6qBly87s5AZ%2BdYDGWSKCNhZ3qyR2Degt6L0o4GFzH5OdMS0yz8PW5Rbk8lLl4BaqsRWvZ%2FO2y0%2F3LyiIs7LUQnIz%2BDmziLJxgaOxBAdXgmFhOPV4VT1CjqMoem2X16IYuANERZw7pQsYHMek4jUCcdarmPwoSj%2F05%2Fwdzblf0TCMfDqHgNNplJezyJ%2B%2FnUWVbcWUI4o9PIERO1hLNFyamAQTf0YnEZ36mR%2BxcGKfO4%2Bm34mTamVUoer3Qo5r7LRkiqNAJnkDyEzB%2BuFZ4E5YPjYh%2B5hlKU2%2FbDPifDvHEfRx2IAksjGA61ztSZgkY15scKwvGa4T8HMcJPmTn0W5L0NUkuaw43VFVeXJvMBWaYML71I3OV%2FNh4MB2U%2BEycLsdGuObAMWIctMncyN5dfdnUdjcDC2WeiStlyb47%2BMLrzngsnJ5N%2B2TqXPVnS9SpU9pKOzrGv1eKuBG3sDRncwnFTDzfUl9hVR0mpoZUZzqwHSEGeb9vRfO16LSoC%2B%2BCP%2Fs2PiPU1w%2BR2T3w%3D%3D&SigAlg=http%3A%2F%2Fwww.w3.org%2F2000%2F09%2Fxmldsig%23rsa-sha1&Signature=F6Rh1sOZ2%2FknoXQYJ6JMLQwZRO5GDSAcXF5wD28uCnaLf4sEo0vflx6bqyux3wPNzzXGBVOIyh6bBEoCJATsl6COZrB4SB9jLkLGosIWYBctg6luq2O%2FQwqQbcv5UfzNdUoqh1KyrEtoamM8cav6pMqiY9GKrUbNE1EDNzrIFLC7XwcTq0pMcpiRoMt8TWLnP9dNtiLspv2CJW5c2GmzVAIrOUyURIBPsv5y5nYIILfgGRmJ9YoAY57zDxAw0cWqY401UQcQQ6S3GUnN3gdDcbO0eJPl%2BVFA6d6eUuMLNkNz6UGCzgZxgg%2FFUKYnB43uhUnnz%2BrIJmrIScrOD9NgKw%3D%3D'
+         
         except:
             saml_response = ""
             relay_state = ""
